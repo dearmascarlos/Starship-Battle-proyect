77f7cb05709bf614c06d4df2c58b20beccd62478
carlos <correo.carlosdearmas@gmail.com>
2022-07-19 13:50:59 +0100

    basic-styles-finished

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 
+//FUNCION  constructora board con tablero de jugador y tablero de computer (IA)
+
+//DEFINIR tamaño y numero de las naves
+
+//FUNCION y BOTON de inicio del juego
+
+//FUNCION posicionamiento de las naves (aleatorio)
+
+//FUNCION  PLAYER  que marque su 1ª elección de casilla al comenzar a jugar
+
+//FUNCION que "acote" los turnos (el tablero solo responde cuando es tu turno, no siempre)
+
+//FUNCION  IA  (Arachnids) que cuando acierte en una casilla "STARSHIP HIT":
+//- en la próxima interacción seleccione posiciones alrededor de ésta, en el sentido de las agujas del reloj
+//- y que desde que consigue 2 aciertos seguidos en una dirección, siga jugando en la misma hasta destruir nave
+
+//FUNCION cuadro de diálogo de Reboot Enterprise, en el que aparezcan los mensajes de cada movimiento
+
+//FUNCION cuadro de diálogo de Arachnids, en el que aparezcan los mensajes de cada movimiento realizado
+
+//FUNCION  IA  (arachnids) para elegir sus coordenadas de juego de forma aleatoria
+
+//FUNCION  IA  (Arachnids) para que no se repita ninguna posición de las jugadas antes
+
+//FUNCION marcador grafico para resaltar las naves eliminadas
+
+//FUNCION para determinar VICTORIA y su mensaje en cuadro de diálogo
+
+//FUNCION para determinar DERROTA y su mensaje en cuadro de diálogo
+
