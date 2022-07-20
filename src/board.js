c09ac346d40217d061cc45211675e9a06d1fb116
Jenny-Campo <jenniferseijo89@gmail.com>
2022-07-20 21:50:29 +0100

    generateShip copy from Alvaro´s explanation

--- src/board.js
+++ src/board.js
@@ -4,8 +4,13 @@ function BoardGame(id) {
     this.id = id
     this.canvas = document.getElementById(id)
     self = this
-    this.ship = []
-    
+    this.fleet = {
+        ship2: [],
+        ship3: [],
+        ship4: [],
+        ship5: []
+    }
+
 
 //FUNCION de seleccion de la casilla:   (si pulsas sobre la casilla, muestra posición en consola)
 
@@ -18,52 +23,47 @@ function BoardGame(id) {
                     td.classList.add('vacuum')
             })
         })
-        GenerateShip(4) 
-        GenerateShip(2)
-        GenerateShip(2)
-        GenerateShip(5)
-        
+    }
+
+    this.generateShip= function(longShip) {
+        let randomRow = 0 
+        let randomCol = 0
+
+        for (var i = 0; i < longShip; i++) { 
+            if (i === 0){
+                randomRow =  Math.floor(Math.random() * 10 )   //Genero núm aleatorio [0, 9]
+                randomCol =  Math.floor(Math.random() * (11 - longShip))   //Genero núm aleatorio [0, 9]
+            } else {
+                randomCol += 1
+            }
+
+            self.fleet['ship' + longShip].push({
+                row: randomRow,
+                col: randomCol
+            })  
+        } 
+
+        self.fleet['ship' + longShip].forEach(function(td) {
+            let element1 = document.querySelector('.row' + td.row + ' .col' + td.col)
+            element1.classList.add('starship') 
+        })
+
     }
 
     this.startBoard = function() { 
         this.createCellInteraction() 
+        for (var i= 2; i < 6 ; i++ {
+            this.generateShip(i)
+        } 
     }
 } 
 
 
+//DEFINIR tamaño y numero de las naves (function GenerateShip)
 
-//DEFINIR tamaño y numero de las naves
-
-function generateShip() {
-    let celda = []
-    let numShips = 1
-    let randomRow = 0
-    let randomCol = 0
-
-    for(let i=0; i < numShips; i++){
-        randomRow =  Math.floor(Math.random() * 9)   //Genero núm aleatorio [0, 9]
-        randomCol =  Math.floor(Math.random() * 9)   //Genero núm aleatorio [0, 9]
-        if(randomCol < 9 && randomRow < 9) {
-            celda.push({
-                row: randomRow,
-                col: randomCol
-            })
-        }else{
-            randomRow =  Math.floor(Math.random() * 8)   //Genero núm aleatorio [0, 8]
-            randomCol =  Math.floor(Math.random() * 8)   //Genero núm aleatorio [0, 8]
-            celda.push({
-                row: randomRow,
-                col: randomCol
-            })
-            
-        }
-    }
-    celda.forEach(function(td){
-
-//function generateShip()
-//DEFINIR tamaño y numero de las naves
-
-/*function generateShip(longShip) {
+//-------------------------------------------------------------------------------------------------------------
+/*hemos cambiado esta función a una función constructora (línea 62)
+function generateShip(longShip) {
     let randomRow = 0 
     let randomCol = 0
        for (var i = 0; i < longShip; i++) { 
@@ -78,65 +78,62 @@ function generateShip() {
                 col: randomCol
             })       
         }        
-
     self.ship.forEach(function(td) {
       let element = document.querySelector('.row' + td.row + ' .col' + td.col)
       element.classList.add('starship') 
-
     })  
     console.log(self.ship)
 }
+//---------------------------------------------------------------------------------------------------------------
 */
 
-function GenerateShip(longShip) {
-    this.randomRow = 0 
-    this.randomColrandomCol = 0
-       for (var i = 0; i < longShip; i++) { 
-            if (i === 0){
-                randomRow =  Math.floor(Math.random() * 10 )   //Genero núm aleatorio [0, 9]
-                randomCol =  Math.floor(Math.random() * (11 - longShip))   //Genero núm aleatorio [0, 9]
-            }else{
-                randomCol += 1
-            }
-            self.ship.push({
-                row: randomRow,
-                col: randomCol
+// function GenerateShip(longShip) {
+//     this.randomRow = 0 
+//     this.randomCol = 0
 
-            })
-        }    
-       // }else{  no estamos encontrando el sentido a esta parte
-       //    randomRow =  Math.floor(Math.random() * 8)   //Genero núm aleatorio [0, 8]
-       //    randomCol =  Math.floor(Math.random() * 8)   //Genero núm aleatorio [0, 8]
-       //     self.ship.push({
-       //         row: randomRow,
-       //         col: randomCol
-       //     })
+//     // nave en la primera llamada a GenerateShip
+//        for (var i = 0; i < longShip; i++) { 
+//             if (i === 0){
+//                 randomRow =  Math.floor(Math.random() * 10 )   //Genero núm aleatorio [0, 9]
+//                 randomCol =  Math.floor(Math.random() * (11 - longShip))   //Genero núm aleatorio [0, 9]
+//             } else {
+//                 randomCol += 1
+//             }
+
+//             self.ship1.push({
+//                 row: randomRow,
+//                 col: randomCol
+//             })  
+//         } 
+
+//         self.ship1.forEach(function(td) {
+//         let element1 = document.querySelector('.row' + td.row + ' .col' + td.col)
+//         element1.classList.add('starship') 
+//         })
         
-    //}
+//     // naves del resto de llamadas a Generateship
+//         for (var i = 0; i < longShip; i++) {
+//             if (i >= 1 && !self.ship1) {
+//                 randomRow =  Math.floor(Math.random() * 10 )   //Genero núm aleatorio [0, 9]
+//                 randomCol =  Math.floor(Math.random() * (11 - longShip))   //Genero núm aleatorio [0, 9]
+//             } else {
+//                 randomCol += 1
+//             } 
     
-    self.ship.forEach(function(td){
+//             self.ship2.push({
+//                 row: randomRow,
+//                 col: randomCol
+//             })
+//         }
 
-        let element = document.querySelector('.row' + td.row + ' .col' + td.col)
-        element.classList.add('starship') 
+//     //     self.ship2.forEach(function(td) {
+//     //     let element2 = document.querySelector('.row' + td.row + ' .col' + td.col)
+//     //     element2.classList.add('starship') 
+//     // })  
 
-        element = document.querySelector('.row' + td.row + ' .col' + (td.col + longship -1))
-        element.classList.add('starship')
-    })       
-    
-
-    self.ship.forEach(function(td) {
-      let element = document.querySelector('.row' + td.row + ' .col' + td.col)
-      element.classList.add('starship') 
-    })  
-
-    console.log(self.ship)
-
-}
-
-// nuevo push //
-
-
-
+          
+//     console.log(self.ship1)
+//     console.log(self.ship2)
 
 
 
