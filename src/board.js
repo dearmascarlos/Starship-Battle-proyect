2404b4fbb516b5f37d9da08c378e5a486ae1c60c
carlos <correo.carlosdearmas@gmail.com>
2022-07-20 21:47:21 +0100

    generateShip part1 finished

--- src/board.js
+++ src/board.js
@@ -4,7 +4,13 @@ function BoardGame(id) {
     this.id = id
     this.canvas = document.getElementById(id)
     self = this
-    this.ship = []
+    this.fleet = {
+        ship2: [],
+        ship3: [],
+        ship4: [],
+        ship5: []
+    }
+
     
 
 //FUNCION de seleccion de la casilla:   (si pulsas sobre la casilla, muestra posición en consola)
@@ -17,71 +23,46 @@ function BoardGame(id) {
                    console.log(row, col)
                     td.classList.add('vacuum')
             })
-        })
-        GenerateShip(4) 
-        GenerateShip(2)
-        GenerateShip(2)
-        GenerateShip(5)
-        
+        })    
+    }
+
+    this.generateShip = function(longShip) {
+        let randomRow = 0 
+        let randomCol = 0
+        for (var i = 0; i < longShip; i++) { 
+                if (i === 0) {
+                    randomRow =  Math.floor(Math.random() * 10 )   //Genero núm aleatorio [0, 9]
+                    randomCol =  Math.floor(Math.random() * (11 - longShip))   //Genero núm aleatorio [0, 9]
+                } else {
+                    randomCol += 1
+                }
+                console.log(self.fleet['ship' + longShip])
+                self.fleet['ship' + longShip].push({
+                    row: randomRow,
+                    col: randomCol
+                })       
+            } 
+            self.fleet['ship' + longShip].forEach(function(td) {
+                console.log(td)
+            let element = document.querySelector('.row' + td.row + ' .col' + td.col)
+            element.classList.add('starship') 
+
+         })  
     }
 
     this.startBoard = function() { 
         this.createCellInteraction() 
+        for(var i = 2; i < 6; i++){
+            this.generateShip(i)
+        }
     }
 } 
 
-//function generateShip()
-//DEFINIR tamaño y numero de las naves
 
-/*function generateShip(longShip) {
-    let randomRow = 0 
-    let randomCol = 0
-       for (var i = 0; i < longShip; i++) { 
-            if (i === 0){
-                randomRow =  Math.floor(Math.random() * 10 )   //Genero núm aleatorio [0, 9]
-                randomCol =  Math.floor(Math.random() * (10 - longShip))   //Genero núm aleatorio [0, 9]
-            }else{
-                randomCol += 1
-            }
-            self.ship.push({
-                row: randomRow,
-                col: randomCol
-            })       
-        }        
 
-    self.ship.forEach(function(td) {
-      let element = document.querySelector('.row' + td.row + ' .col' + td.col)
-      element.classList.add('starship') 
 
-    })  
-    console.log(self.ship)
-}
-*/
+    
 
-function GenerateShip(longShip) {
-    this.randomRow = 0 
-    this.randomColrandomCol = 0
-       for (var i = 0; i < longShip; i++) { 
-            if (i === 0){
-                randomRow =  Math.floor(Math.random() * 10 )   //Genero núm aleatorio [0, 9]
-                randomCol =  Math.floor(Math.random() * (11 - longShip))   //Genero núm aleatorio [0, 9]
-            }else{
-                randomCol += 1
-            }
-            self.ship.push({
-                row: randomRow,
-                col: randomCol
-            })       
-        } 
-    self.ship.forEach(function(td) {
-      let element = document.querySelector('.row' + td.row + ' .col' + td.col)
-      element.classList.add('starship') 
-
-    })  
-    console.log(self.ship)
-}
-
-// nuevo push //
 
 
 
